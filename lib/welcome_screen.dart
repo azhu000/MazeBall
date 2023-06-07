a87147037368570a0ca87278b0b2b1f5a012f812
azhu000 <azhu000@citymail.cuny.edu>
2023-06-07 18:36:03 -0400

    fixed the button locations

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -10,43 +10,58 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            "Welcome to MYAPP",
-            style: TextStyle(fontSize: (screenWidth * 0.1)),
-            textAlign: TextAlign.center,
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: screenHeight * 0.2),
+              Text(
+                "Welcome to MYAPP",
+                style: TextStyle(
+                    fontSize: (screenWidth * 0.1),
+                    backgroundColor: Colors.amber),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: screenHeight * 0.15),
+              TextButton(
+                style: TextButton.styleFrom(
+                    fixedSize: Size(screenWidth * 0.25, screenWidth * 0.08),
+                    backgroundColor: Colors.lightBlue,
+                    foregroundColor: Colors.black),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const LoginPage(title: 'LoginPage');
+                  }));
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: (screenWidth * 0.05)),
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.03),
+              TextButton(
+                style: TextButton.styleFrom(
+                    fixedSize: Size(screenWidth * 0.25, screenWidth * 0.08),
+                    // padding: const EdgeInsets.all(10),
+                    backgroundColor: Colors.lightBlue,
+                    foregroundColor: Colors.black),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const RegisterPage(title: 'RegisterPage');
+                  }));
+                },
+                child: Text('Register',
+                    style: TextStyle(fontSize: (screenWidth * 0.05))),
+              )
+            ],
           ),
-          TextButton(
-            style: TextButton.styleFrom(padding: const EdgeInsets.all(10)),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const LoginPage(title: 'LoginPage');
-              }));
-            },
-            child: Text(
-              'Login',
-              style: TextStyle(fontSize: (screenWidth * 0.05)),
-            ),
-          ),
-          TextButton(
-            style: TextButton.styleFrom(padding: const EdgeInsets.all(10)),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const RegisterPage(title: 'RegisterPage');
-              }));
-            },
-            child: Text('Register',
-                style: TextStyle(fontSize: (screenWidth * 0.05))),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
