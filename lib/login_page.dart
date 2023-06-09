b87902814209e8e02709079512c9117375c9758d
Hyemin.Shin <sshin.hyemin@gmail.com>
2023-06-09 11:25:15 +0900

    login updated

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  final String hint;
-  final Function validator;
-  final Function onSaved;
-  final bool isPassword;
-  final bool isEmail;
+   final String title;
+  // final String hint;
+  // final Function validator;
+  // final Function onSaved;
+  // final bool isPassword;
+  // final bool isEmail;
 
-  LoginPage({
-    this.hint,
-    this.validator,
-    this.onSaved,
-    this.isPassword = false,
-    this.isEmail = false,
-  });
+  // LoginPage({
+  //   this.hint,
+  //   this.validator,
+  //   this.onSaved,
+  //   this.isPassword = false,
+  //   this.isEmail = false,
+  // });
 
   @override
   Widget build(BuildContext context) {
