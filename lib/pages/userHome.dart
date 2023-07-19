f4f745939147f90c85bfc617c6c5b8f42d967070
kianeefondo <k.efondo03@gmail.com>
2023-07-19 18:02:02 -0400

    Toolbar

---
+++ lib/pages/userHome.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UserHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Home Page',
+        style: TextStyle(fontSize: 50),
+      ),
+    );
+  }
+}
