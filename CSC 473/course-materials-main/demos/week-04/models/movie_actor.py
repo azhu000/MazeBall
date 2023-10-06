908d3c17454887ec86cdbc811ecf7b195430bd07
azhu000 <azhu000@citymail.cuny.edu>
2023-10-05 21:26:54 -0400

    removed accidental folder add

--- CSC 473/course-materials-main/demos/week-04/models/movie_actor.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, ForeignKey
-
-from database import db
-
-MovieActor = db.Table(
-    "movie_actors",
-    Column("actor_id", ForeignKey("actors.id"), primary_key=True),
-    Column("movie_id", ForeignKey("movies.id"), primary_key=True),
-)
